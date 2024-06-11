8054739aa328538029da29892c0aa9b706f5cda9
WilliamZeng <itswilliamzeng@gmail.com>
2024-06-11 02:09:26 -0400

    promises

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,22 +1,60 @@
-const resolvedWrapper = () => {
+//ShortCut
+// const resolvedWrapper = (value) => {
+// 	return Promise.resolve(value);
+// };
+
+//New Promise Way
+const resolvedWrapper = (value) => {
+	return new Promise((resolve, reject) => {
+		resolve(value);
+	});
+	// return Promise.resolve(value);
 };
 
-const rejectedWrapper = () => {
+const rejectedWrapper = (value) => {
+	return new Promise((resolve, reject) => {});
 };
 
-const handleResolvedPromise = () => {
+const handleResolvedPromise = (promise) => {
+	promise.then((value) => {
+		console.log(value);
+	});
+	return promise;
 };
 
-const handleResolvedOrRejectedPromise = () => {
-};
+//.then block
 
-const pauseForMs = () => {
+const handleResolvedOrRejectedPromise = (promise) => {
+	return promise
+		.then((value) => {
+			console.log(value);
+			return value;
+		})
+		.catch((error) => {
+			//pass in a promise use a then block then apphend a catch block
+			const errorMessage = `Your error message was: ${error.message}`;
+			console.error(errorMessage);
+			return null;
+		});
+};
+//takes in miliseconds as an argument
+const pauseForMs = (ms) => {
+	// Return a new Promise
+	return new Promise((resolve) => {
+		// Inside the Promise block, this callback function will execute after `ms` milliseconds
+		setTimeout(() => {
+			// After the delay, log a message to indicate the delay duration
+			console.log(`Delayed for ${ms} milliseconds.`);
+			// Resolve the Promise to indicate that the delay is completed
+			resolve();
+		}, ms); // setTimeout will execute after `ms` milliseconds
+	});
 };
 
 module.exports = {
-  resolvedWrapper,
-  rejectedWrapper,
-  handleResolvedPromise,
-  handleResolvedOrRejectedPromise,
-  pauseForMs,
+	resolvedWrapper,
+	rejectedWrapper,
+	handleResolvedPromise,
+	handleResolvedOrRejectedPromise,
+	pauseForMs,
 };
--- src/modify.js
+++ src/modify.js
@@ -1,36 +1,79 @@
 const crypto = require('crypto');
 
 const numsToRGBColor = ([color1, color2, color3]) => {
-  return `rgb(${color1}, ${color2}, ${color3})`;
+	return `rgb(${color1}, ${color2}, ${color3})`;
 };
 
-const getRandomBytes = () => new Promise((resolve, reject) => {
-  crypto.randomFill(new Uint8Array(3), (err, buffer) => {
-    if (err) return reject(err);
-    resolve([...buffer]);
-  });
-});
+const getRandomBytes = () =>
+	new Promise((resolve, reject) => {
+		crypto.randomFill(new Uint8Array(3), (err, buffer) => {
+			if (err) return reject(err);
+			resolve([...buffer]);
+		});
+	});
 
 const return4RandomColors = () => {
-  const colors = [];
-  return getRandomBytes()
-    .then(() => {
-    })
-    .then(() => {
-    })
-    .then(() => {
-    })
-    .then(() => {
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+	const colors = [];
+	return getRandomBytes()
+		.then((buffer1) => {
+			colors.push(numsToRGBColor(buffer1));
+			return getRandomBytes();
+		})
+		.then((buffer2) => {
+			colors.push(numsToRGBColor(buffer2));
+			return getRandomBytes();
+		})
+		.then((buffer3) => {
+			colors.push(numsToRGBColor(buffer3));
+			return getRandomBytes();
+		})
+		.then((buffer4) => {
+			colors.push(numsToRGBColor(buffer4));
+			return colors; //Return the array of colors
+		})
+		.catch((err) => {
+			console.error(err);
+			return []; //Return an empty array in case of error
+		});
 };
+//return4RandomColors function. use the resolved values from getRandomBytes to generate colors.
+//call numsToRGBColor.
 
 return4RandomColors().then(console.log);
 
 module.exports = {
-  numsToRGBColor,
-  getRandomBytes,
-  return4RandomColors,
+	numsToRGBColor,
+	getRandomBytes,
+	return4RandomColors,
 };
+
+/*
+first non-original code
+	const colors = [];
+	return getRandomBytes()
+		.then((resolve) => {
+			crypto.randomFill;
+			console.log(1);
+			return resolve;
+		})
+		.then((resolve) => {
+			crypto.randomFill;
+			console.log(2);
+			return resolve;
+		})
+		.then((resolve) => {
+			crypto.randomFill;
+			console.log(3);
+			return resolve;
+		})
+		.then((resolve) => {
+			crypto.randomFill;
+			console.log(4);
+			return resolve;
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+
+ };
+*/
